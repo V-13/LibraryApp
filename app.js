6611a795fd5661c4bde6544b5e5033c1ea393589
shaluks97 <46239857+shaluks97@users.noreply.github.com>
2019-08-16 16:23:20 +0530

    LibrayApp

--- app.js
+++ app.js
@@ -4,8 +4,35 @@ app.set('view engine','ejs');
 app.use(Express.static(__dirname+"/public"));
 
 nav=[{link:'/books',title:"books"},{link:'/authors',title:"authors"}];
+var book =[
+    {
+      title:"war and peace",
+      genre:"history",
+      author:"author"
+},
+
+{
+    title:"harry potter",
+    genre:"fiction",
+    author:"author"
+},
+
+{
+    title:"wings of fire",
+    genre:"autobiograohy",
+    author:"author"
+},
+
+];
+
 
 app.get('/',(req,res)=>{
-    res.render("index",nav);
+    res.render("index",{nav,title:'library'});
 });
-app.listen(3005);
+app.get('/books',(req,res)=>{
+    res.render("books",{title:"Books",book,h1:"Books"});
+});
+app.listen(3005,()=>{
+    console.log("server running")
+}
+);
