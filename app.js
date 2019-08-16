89491832d5f315b62c1dfa82252ac984bd5b5085
shaluks97 <46239857+shaluks97@users.noreply.github.com>
2019-08-16 14:40:18 +0530

    LibrayApp

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const Express=require('express');
 var app = new Express();
 app.set('view engine','ejs');
 app.use(Express.static(__dirname+"/public"));
+
+nav=[{link:'/books',title:"books"},{link:'/authors',title:"authors"}];
+
 app.get('/',(req,res)=>{
-    res.render("index")
+    res.render("index",nav);
 });
 app.listen(3005);
