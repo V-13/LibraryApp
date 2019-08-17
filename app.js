098cd323516f0e535e32bb59bb0d67ffed2eed55
shaluks97 <46239857+shaluks97@users.noreply.github.com>
2019-08-17 22:06:14 +0530

    library

--- app.js
+++ app.js
@@ -7,23 +7,66 @@ nav=[{link:'/books',title:"books"},{link:'/authors',title:"authors"}];
 var book =[
     {
       title:"war and peace",
+      picture:"/images/war.jpg",
       genre:"history",
       author:"author"
 },
 
 {
     title:"harry potter",
+    picture:"/images/harry.jpg",
     genre:"fiction",
     author:"author"
 },
 
 {
     title:"wings of fire",
-    genre:"autobiograohy",
+    picture:"/images/wings.jpg",
+    genre:"autobiography",
     author:"author"
 },
 
 ];
+
+
+authors =[
+    {
+      title:"Leo Tolstoy",
+      picture:"/images/tol.jpg",
+      genre:"Russian",
+      author:"1847"
+},
+
+{
+    title:"JK rowling",
+    picture:"/images/jk.jpg",
+    genre:"Britian",
+    author:"1965"
+},
+
+{
+    title:"APJ abdul kalam",
+    picture:"/images/wings.jpg",
+    genre:"India",
+    author:"1931"
+},
+
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.get('/',(req,res)=>{
@@ -32,6 +75,24 @@ app.get('/',(req,res)=>{
 app.get('/books',(req,res)=>{
     res.render("books",{title:"Books",book,h1:"Books"});
 });
+
+app.get('/authors',(req,res)=>{
+    res.render("authors",{authors,title:"authors",h1:"Authors"})
+});
+
+
+app.get('/booksingle/:id',(req,res)=>{
+
+    const x= req.params.id;
+    res.render('booksingle',{books:book[x],title:"bookdetails"});
+});
+
+app.get('/authorsingle/:id',(req,res)=>{
+    const x=req.params.id;
+    res.render('authorsingle',{authors:authors[x],title:"authordetails"});
+});
+
+
 app.listen(3005,()=>{
     console.log("server running")
 }
