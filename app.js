09a138ae2556ab58fdb3a9d54ab5712326a5dfff
shaluks97 <46239857+shaluks97@users.noreply.github.com>
2019-08-29 00:23:09 +0530

    curd operations done

--- app.js
+++ app.js
@@ -12,14 +12,15 @@ app.use(bodyParser.json());
 app.use(Express.static(__dirname+"/public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
-Mongoose.connect("mongodb://localhost:27017/bookdb",{ useNewUrlParser: true });
- module.exports = function(req, res, next) {
-      if (!mongoose.Types.ObjectId.isValid(req.params.id))
-      return res.status(404).send('Invalid ID.');
+Mongoose.connect("mongodb://localhost:27017/bookdb");
+// { useNewUrlParser: true });
+//  module.exports = function(req, res, next) {
+//       if (!mongoose.Types.ObjectId.isValid(req.params.id))
+//       return res.status(404).send('Invalid ID.');
     
     
-     next();
-    }
+//      next();
+//     }
 
 const bookModel = Mongoose.model("bookdetails",{
         title:String,
@@ -32,9 +33,33 @@ const bookModel = Mongoose.model("bookdetails",{
 })
 
 
+const authorModel = Mongoose.model("authordetails",{
+    title:String,
+    picture:String,
+    genre:String,
+    author:String
+})
 
 
-nav=[{link:'/books',title:"books"},{link:'/authors',title:"authors"},{link:'/books',title:"viewbooks"},{link:'/addbook',title:"Addbook"}];
+
+
+const UserModel= Mongoose.model("users",{
+    ename:String,
+    eaddress:String,
+    egender:String,
+    edob:String,
+    eemail:String,
+    euname:String,
+    epass:String,
+    ecpass:String
+});
+
+
+
+
+nav=[{link:'/',title:"Home"},{link:'/books',title:"books"},{link:'/authors',title:"authors"},{link:'/books',title:"viewbooks"},{link:'/addbook',title:"Addbook"},
+{link:'/addauthors',title:"Addauthor"},{link:'/updatebooks',title:"updatebooks"},
+{link:'/deletebook',title:"DeleteBooks"},{link:'/register',title:"Registration"}];
 var book123 =[
     {
       title:"war and peace",
@@ -86,35 +111,95 @@ authors =[
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.get('/',(req,res)=>{
+app.get('/index',(req,res)=>{
     res.render("index",{nav,title:'library'});
 });
 
 
-app.get('/authors',(req,res)=>{
-    res.render("authors",{authors,title:"authors",h1:"Authors"})
+app.get('/addauthors',(req,res)=>{
+    res.render('addauthor')
+});
+
+
+app.post('/read1',(req,res)=>{
+    var writer =authorModel(req.body)
+    var result=writer.save((error,data)=>{
+        if (error)
+        {
+            throw error;
+            res.send(error);
+        }
+        else
+        {
+            res.send("<script>alert('Author added successfully')</script><script>window.location.href='/addauthors'</script>");
+        }
+    });
+});
+
+app.get('/authorall',(req,res)=>{
+    var result=authorModel.find((error,data)=>{
+        if(error)
+        {
+            throw error;
+            res.send(error);
+        }
+        else
+        {
+            res.send(data);
+        }
+    });
 });
 
 
 
+ const APIurl3="http://localhost:3005/authorall";
+
+
+
+
+app.get('/authors',(req,res)=>{
+        request(APIurl3,(error,response,body)=>{
+            var writers =JSON.parse(body);
+            console.log(writers);
+            res.render('authors',{authors:writers});
+
+    
+
+    // res.render("authors",{authors,title:"authors",h1:"Authors"})
+});
+});
+
+app.get('/authorone',(req,res)=>{
+    var item= req.query.q;
+    var result=authorModel.findOne({_id:item},(error,data)=>{
+        if(error)
+        {
+            throw error;
+            res.send('error');
+        }
+        else
+        {
+           res.send(data);
+
+        }
+    });
+});
+
+
+const APIurl4="http://localhost:3005/authorone";
+
 
 app.get('/authorsingle/:id',(req,res)=>{
     const x=req.params.id;
-    res.render('authorsingle',{authors:authors[x],title:"authordetails"});
+    request(APIurl4+"/?q="+x,(error,reponse,body)=>{
+        var writer=JSON.parse(body);
+        console.log(writer);
+        res.render('authorsingle',{authors:writer});
+    });
+
+
+    // const x=req.params.id;
+    // res.render('authorsingle',{authors:authors[x],title:"authordetails"});
 });
 
 app.get('/addbook',(rq,rs)=>{
@@ -198,6 +283,165 @@ app.get('/booksingle/:id',(req,res)=>{
   });
 
 });
+
+app.get('/updatebooks',(req,res)=>{
+    res.render('updatebooks')
+})
+
+
+
+
+
+app.get('/updateAPI',(req,res)=>{
+    var item1 = req.query.title;
+    var result = bookModel.updateOne({title:item1},{$set:{author:req.query.author}},(error,data)=>{
+        if(error)
+        {
+            throw error;
+            res.send(error);
+        }
+        else
+        {
+            res.send(data);
+        }
+    })
+})
+
+const APIurl8 = "http://localhost:3005/updateAPI";
+
+app.post('/bookupdate',(req,res)=>{
+    var item1 = req.body.title;
+    var item2 = req.body.author;
+    request(APIurl8+"/?title="+item1+"&&author="+item2,(error,response,body)=>{
+
+        res.send("<script>alert('Book author Updated')</script><script>window.location.href='/updatebooks'</script>");
+
+    })
+});
+
+
+
+
+
+app.get('/deletebook',(req,res)=>{
+    res.render('deletebook');
+});
+
+//An API to delete book
+
+app.get('/deleteAPI',(req,res)=>{
+    var item= req.query.title;
+
+    var result = bookModel.deleteOne({title:item},(error,data)=>{
+        if(error)
+        {
+            throw error;
+            res.send(error);
+        }
+        else
+        {
+            res.send(data);
+        }
+    })
+})
+
+const APIurl6 = "http://localhost:3005/deleteAPI"
+
+app.post('/bookdelete',(req,res)=>{
+    var item = req.body.title;
+
+    request(APIurl6+"/?title="+item,(error,response,body)=>{
+
+        res.send("<script>alert('Book Deleted')</script><script>window.location.href='/deletebooks'</script>");
+
+    })
+});
+
+
+
+
+app.get('/register',(req,res)=>{
+    res.render('register');
+});
+
+app.post('/employeeregister',(req,res)=>{
+    //var items=req.body;
+    //res.render('read',{item:items});
+
+    var user = new UserModel(req.body);
+    var result = user.save((error,data)=>{
+        if(error)
+        {
+            throw error;
+            res.send(error);
+        }
+        else
+        {
+            res.send("<script>alert('User Successfully Inserted')</script><script>window.location.href='/register'</script>");
+        }
+    });
+
+});
+
+
+
+app.get('/',(req,res)=>{
+    res.render('login');
+});
+
+app.get('/loginAPI',(req,res)=>{
+    var item1 = req.query.euname;
+    var item2 = req.query.epass;
+    var result = UserModel.find({$and:[{euname:item1},{epass:item2}]},(error,data)=>{
+        if(error)
+        {
+            throw error;
+            res.send(error);
+        }
+        else
+        {
+            res.send(data);
+        }
+        
+    })
+})
+
+const APIurl5 = "http://localhost:3005/loginAPI"
+
+app.post('/employeelogin',(req,res)=>{
+    var item1 = req.body.euname;
+    var item2 = req.body.epass;
+
+    request(APIurl5+"/?euname="+item1+"&&epass="+item2,(error,response,body)=>{
+        var data = JSON.parse(body);
+
+
+        console.log(data);
+        if(data.length>0){
+
+            if(item1==data[0].euname && item2==data[0].epass)
+            {
+                //res.send(data.euname);
+                res.send("<script>alert('Login Successfull')</script><script>window.location.href='/index'</script>");
+            }
+
+
+        }
+        else{
+            res.send("<script>alert('Login unSuccessfull')</script><script>window.location.href='/'</script>");
+            
+        }
+
+
+    });
+});
+
+
+
+
+
+
+
 
 
 
