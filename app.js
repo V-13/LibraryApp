972df8de10d05ccb6c3108ac4edebf6e2af5fafa
shaluks97 <46239857+shaluks97@users.noreply.github.com>
2019-08-29 09:42:45 +0530

    mongoose cluster added

--- app.js
+++ app.js
@@ -11,8 +11,60 @@ app.set('view engine','ejs');
 app.use(bodyParser.json());
 app.use(Express.static(__dirname+"/public"));
 app.use(bodyParser.urlencoded({extended:true}));
+Mongoose.connect("mongodb+srv://V-13:vivek@cluster0-bgs7t.mongodb.net/casestudy?retryWrites=true&w=majority")
+// Mongoose.connect("mongodb://localhost:27017/bookdb");
 
-Mongoose.connect("mongodb://localhost:27017/bookdb");
+
+// For CORS,Pgm Line no 12 to 29
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200' );
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // { useNewUrlParser: true });
 //  module.exports = function(req, res, next) {
 //       if (!mongoose.Types.ObjectId.isValid(req.params.id))
